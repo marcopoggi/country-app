6b26d2ed1d8f88366331c941de5e286621f26690
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-08 20:13:44 -0300

    set table name and foreign key to lowercase

--- api/src/database/relations.js
+++ api/src/database/relations.js
@@ -4,12 +4,12 @@ const { Country } = require("./models/Country");
 
 //relations-ships N:M
 Country.belongsToMany(Activity, {
-  through: "CountryActivities",
-  foreignKey: "Country_id",
+  through: "country_activities",
+  foreignKey: "country_id",
   timestamps: false,
 });
 Activity.belongsToMany(Country, {
-  through: "CountryActivities",
-  foreignKey: "Activity_id",
+  through: "country_activities",
+  foreignKey: "activity_id",
   timestamps: false,
 });
