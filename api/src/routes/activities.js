d96683490a216b6e2aac6e75e48bcedb60723dc6
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-08 20:20:39 -0300

    ft - '/activities' route configuration and error handling.

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const router = express.Router();
-//model
-const { Activity } = require("../database/models/Activity");
+//controllers
+const {
+  validateCountries,
+  validateActivity,
+} = require("./controllers/validate");
+//queries
+const { setActivity, getMultipleCountries } = require("../database/queries");
 
-router.post("/activities", (req, res) => {
-  const { name, difficulty, duration, season, countries } = req.body;
-  Activity.create({ name, difficulty, duration, season })
-    .then((newActivity) => {
-      res.status(200).send(newActivity.dataValues);
-    })
-    .catch((e) => {
-      res.status(400).send({ error: e?.errors[0]?.message || e.message });
+router.post("/activities", async (req, res) => {
+  try {
+    const { countries } = req.body;
+    const activityCountries = validateCountries(countries);
+    const { name, difficulty, duration, season } = validateActivity(req.body);
+
+    const countriesFromDB = await getMultipleCountries(activityCountries);
+    const response = await setActivity({ name, difficulty, duration, season });
+
+    //set values ​​in table CountryActivities(intermediate).
+    countriesFromDB.forEach(async (country) => {
+      const { act } = response;
+      await act.addCountry(country);
     });
+
+    response.created
+      ? res.status(201).json({ ...response, code: 201 })
+      : res.status(200).json({ ...response, code: 200 });
+  } catch (e) {
+    res.status(400).json({
+      error: e.message,
+      status: 400,
+      statusText: "Invalid fields or server error.",
+    });
+  }
 });
 
 module.exports = router;
