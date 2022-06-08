5f37c0c3f20346be16bb5902dde74735ba1fd448
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-08 20:12:22 -0300

    ft - handle the error if you don't sync the database

--- api/index.js
+++ api/index.js
@@ -3,16 +3,21 @@ const { sequelize } = require("./src/database/db.js"); //instance of Sequelize
 const { initCountries } = require("./src/config/initCountries.js");
 
 // Syncing all the models at once.
-sequelize.sync({ force: false }).then(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connected Database.");
-    //load countries if the country table is empty.
-    await initCountries();
-    server.listen(3001, () => {
-      console.log("Server: Listening at 3001"); // eslint-disable-line no-console
-    });
-  } catch (e) {
-    throw new Error(e.message);
-  }
-});
+sequelize
+  .sync({ force: false })
+  .then(async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connected Database.");
+      //load countries if the country table is empty.
+      await initCountries();
+      server.listen(3001, () => {
+        console.log("Server: Listening at 3001"); // eslint-disable-line no-console
+      });
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  })
+  .catch((e) => {
+    throw new Error("Failed to sync database: " + e.message);
+  });
