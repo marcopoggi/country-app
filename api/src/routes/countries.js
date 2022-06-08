f7dbacebbc6d719265456de79d6f7e477e571c43
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-07 21:33:54 -0300

    ft - creation of '/countries' & '/countries:countryId' routes and error handling

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const router = express.Router();
-//model
+//queries
+const { getCountriesTable, getCountryById } = require("../database/queries");
+//handle errors
+const { COUNTRY_ERRORS } = require("./errors/countries");
 
 router.get("/countries", (req, res) => {
-  //query name
-  res.send("All Countries");
+  try {
+    const { name } = req.query; // .../countries?name="something" -> optional
+    getCountriesTable(name)
+      .then((countries) => {
+        if (countries.length === 0)
+          return res.status(404).json(COUNTRY_ERRORS.empty());
+        return res.status(200).json(countries);
+      })
+      .catch((e) => res.status(502).json(COUNTRY_ERRORS.db_error(e.detail)));
+  } catch (e) {
+    return res.status(502).json(COUNTRY_ERRORS.server_error(e.detail));
+  }
 });
 
 router.get("/countries/:countryId", (req, res) => {
-  const { countryId } = req.params;
-  res.send(`Contry -> ${countryId}`);
+  try {
+    const { countryId } = req.params;
+    getCountryById(countryId)
+      .then((country) => {
+        if (!country)
+          return res.status(404).json(COUNTRY_ERRORS.invalid_id());
+        return res.status(200).json(country);
+      })
+      .catch((e) => res.status(502).json(COUNTRY_ERRORS.db_error(e.detail)));
+  } catch (e) {
+    return res.status(502).json(COUNTRY_ERRORS.server_error(e.detail));
+  }
 });
 
 module.exports = router;
