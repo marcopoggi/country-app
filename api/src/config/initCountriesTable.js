ca61b2fcd7537271bd9a35f2df7dcbc82d8b2614
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-09 00:10:03 -0300

    ft - rename file & safe creation

---
+++ api/src/config/initCountriesTable.js
@@ -0,0 +1,23 @@
+const { getAllCountries } = require("../services/countries");
+const { Country } = require("../database/models/Country");
+
+async function initCountriesTable() {
+  try {
+    const currentCountries = await Country.findAll();
+    //avoid request to the api.
+    if (currentCountries.length > 0 && currentCountries.length <= 250)
+      return console.log("TABLE 'countries': Prepared.");
+
+    const countriesToCreate = await getAllCountries();
+    await Promise.all(
+      countriesToCreate.map(
+        (country) => Country.findOrCreate({ where: { ...country } }) //for safe.
+      )
+    );
+    console.log("TABLE 'countries': All countries has been loaded / Prepared.");
+  } catch (e) {
+    throw new Error(`initializing table "countries" -> ${e.message}`);
+  }
+}
+
+module.exports = { initCountriesTable };
