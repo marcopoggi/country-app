1c2fe2722aa77066a67a9a2d6e0757b2486d8a05
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-09 16:32:04 -0300

    ft - basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Countries App</h1>} />
+      <Route path="countries" element={<h1>Country List</h1>} />
+      <Route path="countries/:id" element={<h1>Country Detail</h1>} />
+      <Route path="activities" element={<h1>Create activity</h1>} />
+      <Route path="*" element={<h1>Page Not Found</h1>} />
+    </Routes>
   );
 }
 
