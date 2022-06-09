c458ca20217c84c6adb32fc09fd20f524532827b
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-09 20:26:40 -0300

    ft - render <Welcome/> component (landing page)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import { Welcome } from "./components/welcome/welcome.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Countries App</h1>} />
-      <Route path="countries" element={<h1>Country List</h1>} />
-      <Route path="countries/:id" element={<h1>Country Detail</h1>} />
-      <Route path="activities" element={<h1>Create activity</h1>} />
-      <Route path="*" element={<h1>Page Not Found</h1>} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="countries" element={<h1>Country List</h1>} />
+        <Route path="countries/:id" element={<h1>Country Detail</h1>} />
+        <Route path="activities" element={<h1>Create activity</h1>} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </div>
   );
 }
 
