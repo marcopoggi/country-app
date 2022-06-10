ba12ec74b9043bdded2e0098657108d82fd60358
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-10 20:26:02 -0300

    feat - render <Countries/>

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { Welcome } from "./pages/Landing/Landing.jsx";
+import { Countries } from "./pages/Countries/Countries.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="countries" element={<h1>Country List</h1>} />
-        <Route path="countries/:id" element={<h1>Country Detail</h1>} />
-        <Route path="activities" element={<h1>Create activity</h1>} />
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-      </Routes>
+      <div className="background">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="countries" element={<Countries />} />
+          <Route path="countries/:id" element={<h1>Country Detail</h1>} />
+          <Route path="activities" element={<h1>Create activity</h1>} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
