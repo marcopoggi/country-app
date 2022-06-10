da1fdc4864c6bd78c99d88fd0b6af20529b95724
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-10 20:23:26 -0300

    fix - remove background

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,5 +1,4 @@
 import {
-  background,
   container,
   presentation,
   subtitle,
@@ -18,18 +17,16 @@ export function Welcome() {
   };
 
   return (
-    <div className={background}>
-      <div className={container}>
-        <div className={presentation}>
-          <Logo titleSize="3em" iconSize="1.05em"></Logo>
-          <h3 className={subtitle}>Discover the World.</h3>
-        </div>
-        <button className={button} onClick={() => handleClick()}>
-          Explore
-        </button>
-        <div className={decoration}>
-          <img src={worldIcon} alt="airplane-world-icon" />
-        </div>
+    <div className={container}>
+      <div className={presentation}>
+        <Logo titleSize="3em" iconSize="1.05em"></Logo>
+        <h3 className={subtitle}>Discover the World.</h3>
+      </div>
+      <button className={button} onClick={() => handleClick()}>
+        Explore
+      </button>
+      <div className={decoration}>
+        <img src={worldIcon} alt="airplane-world-icon" />
       </div>
     </div>
   );
