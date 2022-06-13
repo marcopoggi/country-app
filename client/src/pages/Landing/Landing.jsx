c8466104f2e014e85ba71d625490e21f47f9f686
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 02:14:09 -0300

    refact - remove styles

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,10 +1,3 @@
-import {
-  container,
-  presentation,
-  subtitle,
-  button,
-  decoration,
-} from "./Landing.module.css";
 import worldIcon from "../../assets/img/world.png";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../../components/Logo/Logo";
@@ -17,15 +10,13 @@ export function Welcome() {
   };
 
   return (
-    <div className={container}>
-      <div className={presentation}>
-        <Logo titleSize="3em" iconSize="1.05em"></Logo>
-        <h3 className={subtitle}>Discover the World.</h3>
+    <div>
+      <div>
+        <Logo></Logo>
+        <h3>Discover the World.</h3>
       </div>
-      <button className={button} onClick={() => handleClick()}>
-        Explore
-      </button>
-      <div className={decoration}>
+      <button onClick={() => handleClick()}>Explore</button>
+      <div>
         <img src={worldIcon} alt="airplane-world-icon" />
       </div>
     </div>
