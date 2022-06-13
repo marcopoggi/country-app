4d71aa64a227479d38695bedd9e4054d85d6a6ee
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 02:13:37 -0300

    refact - remove styles

--- client/src/pages/Countries/Countries.jsx
+++ client/src/pages/Countries/Countries.jsx
@@ -1,16 +1,14 @@
 import { Logo } from "../../components/Logo/Logo";
 import { Searcher } from "../../components/Searcher/Searcher";
-import { container,navbar } from "./Countries.module.css";
+import { CountryList } from "../../components/CountryList/CountryList";
 
 export function Countries() {
   return (
-    <div className={container}>
-      <div className={navbar}>
-        <Logo titleSize="2em" iconSize="1.05em"/>
-        <Searcher />
-        {/* Country List */}
-        {/* Pagination Component */}
-      </div>
+    <div>
+      <Logo />
+      <Searcher />
+      <CountryList />
+      {/* Pagination Component */}
     </div>
   );
 }
