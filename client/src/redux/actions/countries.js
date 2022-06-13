6fd65ce39fac87b0eb1a500e9a22c64116c3ac23
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 16:33:03 -0300

    feat - add actions to set a view, filters, and sort

--- client/src/redux/actions/countries.js
+++ client/src/redux/actions/countries.js
@@ -2,10 +2,31 @@ import { getAllCountries as countryService } from "../../services/countries";
 
 export function setCountries() {
   return async function (dispatch) {
-    const { response, error, message } = await countryService();
+    const { response } = await countryService();
     return dispatch({
       type: "@countries/setCountries",
-      payload: { response, error, message },
+      payload: response,
     });
+  };
+}
+
+export function setCountriesToView(countries = []) {
+  return {
+    type: "@countries/setView",
+    payload: countries,
+  };
+}
+
+export function setCountriesByFilters(filters) {
+  return {
+    type: "@countries/setFilter",
+    payload: filters,
+  };
+}
+
+export function setCountriesByOrder(orderBy = "", ascendant = false) {
+  return {
+    type: "@countries/setOrder",
+    payload: { orderBy, ascendant },
   };
 }
