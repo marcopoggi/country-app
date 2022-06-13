a01df1db599c0085ca74e1067a89fcf70c25add7
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 16:41:20 -0300

    feat - add effect to alter view based on order,search,filters

--- client/src/hooks/useCountries.jsx
+++ client/src/hooks/useCountries.jsx
@@ -1,21 +1,44 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { setCountries } from "../redux/actions/countries";
+import { useEffect, useState } from "react";
+import { setCountries, setCountriesToView } from "../redux/actions/countries";
+//handlers
+import {
+  getFilteredCountries,
+  getOrderedCountries,
+} from "../handlers/countries";
 
 export function useCountries() {
-  //redux hooks
-  const { countries, error, message } = useSelector((state) => state.countries);
+  const { countries, countriesToView, error, filters, order } = useSelector(
+    (state) => state.countries
+  );
   const dispatch = useDispatch();
-  //component state
+  const [loading, setLoading] = useState(false);
 
   useEffect(
     function () {
+      setLoading(true);
       if (countries.length === 0) {
         dispatch(setCountries());
+        dispatch(setCountriesToView(countries));
       }
+      dispatch(setCountriesToView(countries));
+      setLoading(false);
     },
-    [countries.length, dispatch]
+    [countries.length, dispatch, countries]
   );
-  return { countries, error, message };
+
+  useEffect(
+    function () {
+      setLoading(true);
+      const filtered = getFilteredCountries(countries, filters);
+      const ordered = getOrderedCountries(filtered, order);
+      dispatch(setCountriesToView(ordered));
+      setLoading(false);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [filters, order]
+  );
+
+  return { countries: countriesToView, error, loading };
 }
