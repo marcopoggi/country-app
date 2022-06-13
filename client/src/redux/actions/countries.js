856e64465f1e8ea16537aef74f8e1dbe34de32b9
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 02:16:37 -0300

    feat - create async action for get countries from the service

--- client/src/redux/actions/countries.js
+++ client/src/redux/actions/countries.js
@@ -1,3 +1,11 @@
-export function addCountry() {
-  
+import { getAllCountries as countryService } from "../../services/countries";
+
+export function setCountries() {
+  return async function (dispatch) {
+    const { response, error, message } = await countryService();
+    return dispatch({
+      type: "@countries/setCountries",
+      payload: { response, error, message },
+    });
+  };
 }
