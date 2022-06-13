c4bf2d4c6113c7e3ad8c97d8bf2cb9fef0cd8f6f
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 16:29:40 -0300

    refactor - always return a response object, in case of error it has an error property

--- client/src/services/countries.js
+++ client/src/services/countries.js
@@ -4,10 +4,9 @@ async function getAllCountries() {
   try {
     const response = await fetch(`${API_URL}/countries`);
     const data = await response.json();
-    if (data?.error) throw Error(data.error);
     return { response: data };
   } catch (e) {
-    return { response: null, error: true, message: e.message };
+    return { response: { error: "Error when trying to get the countries" } };
   }
 }
 
