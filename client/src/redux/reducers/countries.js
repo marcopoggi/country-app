eed04f85e318f6f053a8761a0892ca2b52fe36b8
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 02:17:54 -0300

    feat - handling action type = @countries/setCountries

--- client/src/redux/reducers/countries.js
+++ client/src/redux/reducers/countries.js
@@ -1,12 +1,21 @@
+const LIMIT = 10;
+
 const initialState = {
   countries: [],
+  error: false,
+  message: "",
 };
 
 export function countriesReducer(state = initialState, action) {
   switch (action.type) {
-    case "@countries/get":
-      break;
-
+    case "@countries/setCountries":
+      const { response, error, message } = action.payload;
+      if (!response) return { ...state, error, message };
+      return {
+        ...state,
+        countries: [...response],
+        totalPages: Math.ceil(response.length / LIMIT),
+      };
     default:
       return { ...state };
   }
