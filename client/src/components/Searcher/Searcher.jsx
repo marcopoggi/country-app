2ec7d2a2ec68857b03eadd2576329c60577011f5
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 02:10:24 -0300

    refact - remove logic

--- client/src/components/Searcher/Searcher.jsx
+++ client/src/components/Searcher/Searcher.jsx
@@ -1,16 +1,8 @@
-import searchIcon from "../../assets/img/search.svg";
-import preferencesIcon from "../../assets/img/preferences.svg";
-import {
-  searcher_container,
-  searcher_preferences,
-  searcher_search,
-  icons,
-} from "./Searcher.module.css";
 import { useRef, useState } from "react";
 
 export function Searcher() {
-  const searchInput = useRef(null);
   const [countryToSearch, setCountryToSearch] = useState("");
+  const searchInput = useRef(null);
 
   const handleInput = (e) => {
     let value = e.target.value;
@@ -19,24 +11,21 @@ export function Searcher() {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    searchInput.current.value = "";
-    const trimValue = countryToSearch.trim();
-    if (trimValue !== "") console.log("Buscar -> " + trimValue);
   };
 
   return (
-    <form className={searcher_container}>
+    <form>
       <input
         type="text"
         onChange={handleInput}
         placeholder="Example: Argentina"
         ref={searchInput}
       />
-      <button type="button" className={searcher_preferences}>
-        <img src={preferencesIcon} alt="preference-icon" className={icons} />
+      <button type="button">
+        <img src={null} alt="preferences" />
       </button>
-      <button type="submit" className={searcher_search} onClick={handleSearch}>
-        <img src={searchIcon} alt="search-icon" className={icons} />
+      <button type="submit" onClick={handleSearch}>
+        <img src={null} alt="search" />
       </button>
     </form>
   );
