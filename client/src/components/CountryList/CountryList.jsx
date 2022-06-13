4c06ab17d9223c84bfa5ad232f722ad787a7d54a
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-13 16:55:08 -0300

    feat - render countries - no countries / loader / errorSign

--- client/src/components/CountryList/CountryList.jsx
+++ client/src/components/CountryList/CountryList.jsx
@@ -2,21 +2,29 @@ import { useCountries } from "../../hooks/useCountries";
 import { ErrorSign } from "../ErrorSign/ErrorSign";
 import { Loader } from "../Loader/Loader";
 import { CountryCard } from "../CountryCard/CountryCard";
+import { useEffect, useState } from "react";
 
 export function CountryList() {
-  const { loading, countries, error, message } = useCountries();
+  const { countries, error, loading } = useCountries();
+  const [countriesToView, setCountriesToView] = useState(countries);
 
-  return (
-    <div>
-      {error ? (
-        <ErrorSign message={message} />
-      ) : loading ? (
-        <Loader />
-      ) : (
-        countries.map((country) => (
-          <CountryCard {...country} key={country.name} />
-        ))
-      )}
-    </div>
+  useEffect(
+    function () {
+      setCountriesToView(countries);
+    },
+    [countries]
+  );
+
+  return loading ? (
+    <Loader />
+  ) : error.state ? (
+    <ErrorSign message={error.msg} />
+  ) : countriesToView.length === 0 ? (
+    // add component for when no countries are found.
+    <h2>No countries found</h2>
+  ) : (
+    countriesToView.map((country) => (
+      <CountryCard {...country} key={country.name} />
+    ))
   );
 }
