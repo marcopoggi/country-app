cdcd761e439a73ea2a73d106813ddeffcede5bfc
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-14 01:24:53 -0300

    feat - render Preferences component

--- client/src/pages/Countries/Countries.jsx
+++ client/src/pages/Countries/Countries.jsx
@@ -1,12 +1,14 @@
 import { Logo } from "../../components/Logo/Logo";
 import { Searcher } from "../../components/Searcher/Searcher";
 import { CountryList } from "../../components/CountryList/CountryList";
+import { Preferences } from "../../components/Preferences/Preferences";
 
 export function Countries() {
   return (
     <div>
       <Logo />
       <Searcher />
+      <Preferences />
       <CountryList />
       {/* Pagination Component */}
     </div>
