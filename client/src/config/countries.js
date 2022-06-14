8ea1b4f73f5bfd3e1fc869d1451936185b852501
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-14 01:21:26 -0300

    feat - constant default values ​​for preferences

---
+++ client/src/config/countries.js
@@ -0,0 +1,10 @@
+export const CONTINENTS = [
+  "africa",
+  "americas",
+  "antarctic",
+  "asia",
+  "europe",
+  "oceania",
+];
+export const ORDERS = ["name", "population"];
+export const ACTIVITIES = ["ski", "museum", "climbing", "beach", "paragliding"];
