49bd80313318048285a1e08810fc0039699f1fc0
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-15 17:52:20 -0300

    feat - create a 'custom hook' to get the detail (in local or by request)

---
+++ client/src/hooks/useDetail.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { setDetail } from "../redux/actions/details";
+
+export function useDetail() {
+  const dispatch = useDispatch();
+  const { idOrName } = useParams();
+  const { details, error } = useSelector((state) => state.details);
+  const [loading, setLoading] = useState(false);
+  const [countryDetail, setCountryDetail] = useState({});
+
+  useEffect(
+    function () {
+      setLoading(true);
+      let searchLocalDetail = details.find(
+        ({ id, name }) => name === idOrName || id === idOrName
+      );
+      if (!searchLocalDetail) {
+        dispatch(setDetail(idOrName));
+      } else {
+        setCountryDetail(searchLocalDetail);
+        setLoading(false);
+      }
+    },
+    [idOrName, details, dispatch]
+  );
+
+  return { countryDetail, error, loading };
+}
