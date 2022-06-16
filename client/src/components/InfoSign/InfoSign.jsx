7ffc474fba293cef5051c97d0a46e398bea89934
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-16 19:20:43 -0300

    style - rename file

---
+++ client/src/components/InfoSign/InfoSign.jsx
@@ -0,0 +1,9 @@
+export function InfoSign({ message, title = "Something has wrong", img }) {
+  return (
+    <div>
+      <img src={img} alt="⚠️" />
+      <h3>{title}</h3>
+      <h4>{message}</h4>
+    </div>
+  );
+}
