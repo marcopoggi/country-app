d9ea0feb44b076fc6b4d0dc0c594880e9db03512
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-16 19:21:18 -0300

    style - rename file

--- client/src/components/ErrorSign/ErrorSign.jsx
+++
@@ -1,9 +0,0 @@
-export function ErrorSign({ message, title = "Something has wrong", img }) {
-  return (
-    <div>
-      <img src={img} alt="⚠️" />
-      <h3>{title}</h3>
-      <h4>{message}</h4>
-    </div>
-  );
-}
