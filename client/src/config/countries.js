80714ee26b597114edb663a8d4bef738dc0ff91c
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-16 19:27:05 -0300

    feat - add static seasons / initial form(state,errors)

--- client/src/config/countries.js
+++ client/src/config/countries.js
@@ -8,3 +8,20 @@ export const CONTINENTS = [
 ];
 export const ORDERS = ["name", "population"];
 export const ACTIVITIES = ["ski", "museum", "climbing", "beach", "paragliding"];
+export const SEASONS = ["any", "autumn", "winter", "spring", "summer"];
+
+export const INITIAL_FORM = {
+  name: "",
+  duration: "",
+  difficulty: "",
+  season: "any",
+  selectedCountries: [],
+};
+
+export const INITIAL_FORM_ERRORS = {
+  name: "",
+  duration: "",
+  difficulty: "",
+  season: "",
+  selectedCountries: "",
+};
