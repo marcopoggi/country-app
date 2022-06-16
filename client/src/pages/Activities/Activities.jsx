fd44ca554ec6e7bce1ef640f6156466eb09941f0
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-16 19:36:11 -0300

    feat - render error / loader / form to create activity | edit imports

--- client/src/pages/Activities/Activities.jsx
+++ client/src/pages/Activities/Activities.jsx
@@ -1,14 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { CreateActivity } from "../../components/CreateActivity/CreateActivity";
+import { useCountries } from "../../hooks/useCountries";
+import { InfoSign } from "../../components/InfoSign/InfoSign";
+import { Loader } from "../../components/Loader/Loader";
 
 export function Activities() {
+  const { error, countries, loading } = useCountries(true);
   const navigate = useNavigate();
 
   return (
     <div>
       <button onClick={() => navigate(-1)}>Back</button>
       <h1>Create activity</h1>
-      <CreateActivity />
+      {error.state ? (
+        <InfoSign />
+      ) : loading ? (
+        <Loader />
+      ) : (
+        <CreateActivity countries={countries} />
+      )}
     </div>
   );
 }
