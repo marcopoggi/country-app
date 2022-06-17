cd5b3d699a28b6fbfedabd264186a16ccd69bb1a
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-17 16:47:58 -0300

    styles - edit style

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,10 +1,12 @@
+import landingLogo from "../../assets/img/landingIcon.png";
 //styles
 import { words, earth } from "./Logo.module.css";
 
 export function Logo() {
   return (
     <h1 className={words}>
-      C<span className={earth}>🌍</span>untry App
+      C<img src={landingLogo} alt="airplane-world-icon" className={earth} />
+      untry App
     </h1>
   );
 }
