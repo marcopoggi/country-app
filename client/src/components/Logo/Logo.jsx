d106910e90e24e182fe8d7e0438bfa6652ca96ba
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-17 00:54:44 -0300

    feat - create logo style

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,7 +1,10 @@
+//styles
+import { words, earth } from "./Logo.module.css";
+
 export function Logo() {
   return (
-    <h1>
-      C<span>🌍</span>untry App
+    <h1 className={words}>
+      C<span className={earth}>🌍</span>untry App
     </h1>
   );
 }
