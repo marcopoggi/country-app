c43f603259da913dc31fc42a6ccdd89b53fd1544
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-17 00:55:05 -0300

    feat - landing page styles

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,6 +1,13 @@
 import worldIcon from "../../assets/img/world.png";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../../components/Logo/Logo";
+//styles
+import {
+  container,
+  decoration,
+  welcome_words,
+  welcome_btn,
+} from "./Landing.module.css";
 
 export function Welcome() {
   const navigate = useNavigate();
@@ -10,13 +17,16 @@ export function Welcome() {
   };
 
   return (
-    <div>
-      <div>
+    <div className={container}>
+      <div className={welcome_words}>
         <Logo></Logo>
-        <h3>Discover the World.</h3>
+        <h3>Discover the World</h3>
+        <button className={welcome_btn} onClick={() => handleClick()}>
+          Explore
+        </button>
       </div>
-      <button onClick={() => handleClick()}>Explore</button>
-      <div>
+
+      <div className={decoration}>
         <img src={worldIcon} alt="airplane-world-icon" />
       </div>
     </div>
