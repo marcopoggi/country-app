c52ffbbf76e58e04f2f2ad2b00fcf7d2de653e1e
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-17 16:48:31 -0300

    refact - reset design

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,12 +1,11 @@
-import worldIcon from "../../assets/img/world.png";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../../components/Logo/Logo";
 //styles
 import {
   container,
-  decoration,
   welcome_words,
-  welcome_btn,
+  welcome_container,
+  welcome_words_btn,
 } from "./Landing.module.css";
 
 export function Welcome() {
@@ -18,16 +17,14 @@ export function Welcome() {
 
   return (
     <div className={container}>
-      <div className={welcome_words}>
-        <Logo></Logo>
-        <h3>Discover the World</h3>
-        <button className={welcome_btn} onClick={() => handleClick()}>
-          Explore
-        </button>
-      </div>
-
-      <div className={decoration}>
-        <img src={worldIcon} alt="airplane-world-icon" />
+      <div className={welcome_container}>
+        <div className={welcome_words}>
+          <Logo></Logo>
+          <h3>Discover the World</h3>
+          <button className={welcome_words_btn} onClick={() => handleClick()}>
+            Explore
+          </button>
+        </div>
       </div>
     </div>
   );
