75c11dd54b5ed97650a2b2b99d42d0df2b5bd441
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-17 20:08:26 -0300

    feat - define containers & classnames / add handler open/close preferences

--- client/src/pages/Countries/Countries.jsx
+++ client/src/pages/Countries/Countries.jsx
@@ -2,14 +2,28 @@ import { Logo } from "../../components/Logo/Logo";
 import { Searcher } from "../../components/Searcher/Searcher";
 import { CountryList } from "../../components/CountryList/CountryList";
 import { Preferences } from "../../components/Preferences/Preferences";
+import { useState } from "react";
+//styles
+import {
+  open_prefs,
+  close_prefs,
+  container,
+  countries_container,
+} from "./Countries.module.css";
 
 export function Countries() {
+  const [preferences, setPreferences] = useState(false);
+
   return (
-    <div>
-      <Logo />
-      <Searcher />
-      <Preferences />
-      <CountryList />
+    <div className={container}>
+      <div className={countries_container}>
+        <Logo />
+        <Searcher handlePreferences={() => setPreferences(!preferences)} />
+        <div className={preferences ? open_prefs : close_prefs}>
+          <Preferences />
+        </div>
+        <CountryList />
+      </div>
     </div>
   );
 }
