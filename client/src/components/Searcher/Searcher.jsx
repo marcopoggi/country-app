f92156057f6d5346e97a4d5aab9e9c888c9ee814
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-17 20:04:23 -0300

    feat - searcher styles

--- client/src/components/Searcher/Searcher.jsx
+++ client/src/components/Searcher/Searcher.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCountriesByFilters } from "../../redux/actions/countries";
+//styles
+import {
+  searcher,
+  searcher_input,
+  searcher_prefs,
+  searcher_functions,
+} from "./Searcher.module.css";
+import preferences_icon from "../../assets/img/preference.png";
 
-export function Searcher() {
+export function Searcher({ handlePreferences }) {
   const disptach = useDispatch();
   const [countryToSearch, setCountryToSearch] = useState("");
   const searchInput = useRef(null);
@@ -20,13 +28,19 @@ export function Searcher() {
   );
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input
-        type="text"
-        onChange={handleInput}
-        placeholder="Example: Argentina"
-        ref={searchInput}
-      />
+    <form onSubmit={(e) => e.preventDefault()} className={searcher}>
+      <div className={searcher_functions}>
+        <input
+          type="text"
+          onChange={handleInput}
+          placeholder="Example: Argentina"
+          ref={searchInput}
+          className={searcher_input}
+        />
+        <button className={searcher_prefs} onClick={handlePreferences}>
+          <img src={preferences_icon} alt="preferences" />
+        </button>
+      </div>
     </form>
   );
 }
