78451543625c6e8d6f1cdfb62936f80ea6482c89
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-19 19:17:09 -0300

    feat - loader styles

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,7 +1,11 @@
+//styles
+import { loader_container, loader_spinner } from "./Loader.module.css";
+
 export function Loader() {
   return (
-    <div>
-      <h2>Loading...</h2>
+    <div className={loader_container}>
+      <div className={loader_spinner}></div>
+      <h3>Loading</h3>
     </div>
   );
 }
