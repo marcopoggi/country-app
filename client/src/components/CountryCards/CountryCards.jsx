85c6fd18e4d8481adbd52929fc19d46dd569517b
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-19 02:28:00 -0300

    feat - add classnames / basic grid template

--- client/src/components/CountryCards/CountryCards.jsx
+++ client/src/components/CountryCards/CountryCards.jsx
@@ -1,10 +1,12 @@
 import { CountryCard } from "../CountryCard/CountryCard";
+//styles
+import { container_cards } from "./CountryCards.module.css";
 
 export function CountryCards({ countries, page }) {
   const from = page * 10 - 10;
   const to = page * 10;
   return (
-    <div style={{ display: "flex" }}>
+    <div className={container_cards}>
       {countries.slice(from, to).map((country) => (
         <CountryCard {...country} key={country.name} />
       ))}
