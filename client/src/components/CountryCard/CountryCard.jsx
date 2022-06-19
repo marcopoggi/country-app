f425f8a4f61d5788d243dbcef11fd9b99e770207
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-19 03:32:50 -0300

    feat - add background img / classnames / population info

--- client/src/components/CountryCard/CountryCard.jsx
+++ client/src/components/CountryCard/CountryCard.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+//styles
+import { card, card_basic_info } from "./CountryCard.module.css";
 
 export function CountryCard({
   continent,
@@ -9,12 +11,15 @@ export function CountryCard({
 }) {
   return (
     <Link
-      style={{ background: "grey", margin: "10px", width: "200px", textDecoration:"none", color:"#000"}}
       to={name}
+      style={{ backgroundImage: `url(${flag_image})` }}
+      className={card}
     >
-      <img src={flag_image} alt={`${name}-flag`} style={{ width: "100px" }} />
-      <h2>{name}</h2>
-      <h3>{continent}</h3>
+      <div className={card_basic_info}>
+        <h2>{name}</h2>
+        <h3>{continent}</h3>
+        <h4>population: {population}</h4>
+      </div>
     </Link>
   );
 }
