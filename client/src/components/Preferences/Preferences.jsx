5048bfec2aa96ea48596a7ebacd9cbd7a44cc35d
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-18 23:02:50 -0300

    refact - change handlerOrder to act as an toggle btn / import icons / add classnames

--- client/src/components/Preferences/Preferences.jsx
+++ client/src/components/Preferences/Preferences.jsx
@@ -6,6 +6,18 @@ import {
 } from "../../redux/actions/countries";
 import { PreferenceSection } from "../PreferenceSection/PreferenceSection";
 import { CONTINENTS, ORDERS, ACTIVITIES } from "../../config/countries";
+//styles
+import {
+  preferences_container,
+  preferences_back_btn,
+  preferences_header,
+  preferences_settings,
+  preferences_settings_apply,
+} from "./Preferences.module.css";
+import back_btn from "../../assets/img/back_btn.png";
+import continent_img from "../../assets/img/continent.png";
+import activities_img from "../../assets/img/activities.png";
+import sort_img from "../../assets/img/sort.png";
 
 export function Preferences({ handlePreferences }) {
   const dispatch = useDispatch();
@@ -13,7 +25,7 @@ export function Preferences({ handlePreferences }) {
   const [order, setOrder] = useState({ sortBy: "", descendent: false });
   const [newPreferences, setNewPreferences] = useState(false);
 
-  //filter preferences [continent,activities]
+  //filter preferences [continent,activities
   const handleFilters = (e) => {
     const { name, value, checked } = e.target;
     if (checked) setFilters({ ...filters, [name]: [...filters[name], value] });
@@ -26,10 +38,11 @@ export function Preferences({ handlePreferences }) {
 
   //order preferences [name,population,ascendant]
   const handleOrder = (e) => {
-    const { name, value, checked } = e.target;
+    const { name, value } = e.target;
     if (name === "descendent") {
-      setOrder({ ...order, [name]: checked });
+      setOrder({ ...order, descendent: !order.descendent });
     } else setOrder({ ...order, [name]: value });
+
     setNewPreferences(true);
   };
 
@@ -44,31 +57,41 @@ export function Preferences({ handlePreferences }) {
     }
   };
 
-  //reset filters
-  useEffect(function () {
+  const clearPreferences = () => {
     setFilters({ continent: [], activities: [] });
     setOrder({ sortBy: "", descendent: false });
+  };
+
+  //reset filters
+  useEffect(function () {
+    clearPreferences();
     dispatch(setCountriesByFilters(filters));
     dispatch(setCountriesByOrder(order));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div>
-      <button onClick={handlePreferences}>Close</button>
-      <form onSubmit={applyPreferences}>
+    <div className={preferences_container}>
+      <div className={preferences_header}>
+        <button onClick={handlePreferences} className={preferences_back_btn}>
+          <img src={back_btn} alt="Back" />
+        </button>
         <h2>Preferences</h2>
+      </div>
+      <form onSubmit={applyPreferences} className={preferences_settings}>
         <PreferenceSection
           data={CONTINENTS}
           handler={handleFilters}
           name="continent"
           title="Continent"
+          icon={continent_img}
         />
         <PreferenceSection
           data={ACTIVITIES}
           handler={handleFilters}
           name="activities"
           title="Activities"
+          icon={activities_img}
         />
         <PreferenceSection
           data={ORDERS}
@@ -76,15 +99,15 @@ export function Preferences({ handlePreferences }) {
           name="sortBy"
           title="Sort by"
           type="radio"
+          isDescendent={order.descendent}
           defaultValue={true}
+          icon={sort_img}
         />
-        <div>
-          <input type="checkbox" name="descendent" onClick={handleOrder} />
-          <label htmlFor="descendent">Descendent</label>
+        <div className={preferences_settings_apply}>
+          <button disabled={!newPreferences} onClick={handlePreferences}>
+            Apply
+          </button>
         </div>
-        <button disabled={!newPreferences} onClick={handlePreferences}>
-          Apply
-        </button>
       </form>
     </div>
   );
