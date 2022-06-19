e36610be9dd24fc56a7b386d3e2400dcb8849e8a
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-18 23:05:53 -0300

    refact - add a sort button in case the name property is 'sortBy' / add classnames for styles

--- client/src/components/PreferenceSection/PreferenceSection.jsx
+++ client/src/components/PreferenceSection/PreferenceSection.jsx
@@ -1,16 +1,50 @@
+//styles
+import {
+  preference_section_container,
+  preference_section_inputs,
+  input_check_radio,
+  preference_header,
+  descendent_btn,
+  descendent_btn_true,
+  descendent_btn_false,
+} from "./PreferenceSection.module.css";
+
 export function PreferenceSection({
+  icon,
   data,
   name,
   title,
   handler,
   type = "checkbox",
   defaultValue = false,
+  isDescendent,
 }) {
   return (
-    <div>
-      <h4>{title}</h4>
+    <div className={preference_section_container}>
+      <div className={preference_header}>
+        <h3>{title}</h3>
+        {name !== "sortBy" ? (
+          <img src={icon} alt="section-img" />
+        ) : (
+          <>
+            <button
+              type="button"
+              name="descendent"
+              onClick={handler}
+              className={
+                isDescendent
+                  ? `${descendent_btn} ${descendent_btn_true}`
+                  : `${descendent_btn} ${descendent_btn_false}`
+              }
+            >
+              <img src={icon} alt="section-img" name="descendent" />
+            </button>
+          </>
+        )}
+      </div>
+
       {data.map((val, index) => (
-        <div key={val}>
+        <div key={val} className={preference_section_inputs}>
           {
             //if you want to set a default value, select the first one.
             defaultValue && index === 0 ? (
@@ -18,15 +52,23 @@ export function PreferenceSection({
                 type={type}
                 name={name}
                 value={val}
+                id={val}
                 onClick={handler}
                 defaultChecked
+                className={input_check_radio}
               />
             ) : (
-              <input type={type} name={name} value={val} onClick={handler} />
+              <input
+                type={type}
+                name={name}
+                value={val}
+                id={val}
+                onClick={handler}
+                className={input_check_radio}
+              />
             )
           }
-
-          <label htmlFor={name}>{val}</label>
+          <label htmlFor={val}>{val}</label>
         </div>
       ))}
     </div>
