e2f7cde6171a63c1b6cae0c052cc966541629133
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-19 18:39:12 -0300

    feat - default image / styles

--- client/src/components/InfoSign/InfoSign.jsx
+++ client/src/components/InfoSign/InfoSign.jsx
@@ -1,6 +1,14 @@
-export function InfoSign({ message, title = "Something has wrong", img }) {
+//styles
+import { container_infosign } from "./InfoSign.module.css";
+import error_default from "../../assets/img/error-icon.png";
+
+export function InfoSign({
+  message = "",
+  title = "Something has wrong",
+  img = error_default,
+}) {
   return (
-    <div>
+    <div className={container_infosign}>
       <img src={img} alt="⚠️" />
       <h3>{title}</h3>
       <h4>{message}</h4>
