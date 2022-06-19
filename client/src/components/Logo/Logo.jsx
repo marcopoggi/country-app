bf7ef735a5fe9719eebb73e999ed415786257ebf
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-19 03:26:10 -0300

    style - no translate

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import { words, earth } from "./Logo.module.css";
 
 export function Logo() {
   return (
-    <h1 className={words}>
+    <h1 className={words} translate="no">
       C<img src={landingLogo} alt="airplane-world-icon" className={earth} />
       untry App
     </h1>
