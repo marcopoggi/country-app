2865831d170c184d22f88ce0065823e4df87bc56
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-20 02:36:29 -0300

    feat - add class names / set containers / add imports

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -2,21 +2,38 @@ import { InfoSign } from "../../components/InfoSign/InfoSign";
 import { Loader } from "../../components/Loader/Loader";
 import { CountryDetail } from "../../components/CountryDetail/CountryDetail";
 import { useDetail } from "../../hooks/useDetail";
-import { Logo } from "../../components/Logo/Logo";
+import { useNavigate } from "react-router-dom";
+//styles
+import {
+  container_detail,
+  container_background,
+  container_detail_header,
+} from "./Detail.module.css";
+import back_btn from "../../assets/img/back_btn.png";
 
 export function Detail() {
-  const { countryDetail, error, loading } = useDetail();
+  const navigate = useNavigate();
+  const { countryDetail, error, loading, param } = useDetail();
 
   return (
-    <div>
-      <Logo />
-      {error.state ? (
-        <InfoSign />
-      ) : loading ? (
-        <Loader />
-      ) : (
-        <CountryDetail info={countryDetail} />
-      )}
+    <div className={container_detail}>
+      <div className={container_background}>
+        <div className={container_detail_header}>
+          <button className={back_btn} onClick={() => navigate(-1)}>
+            <img src={back_btn} alt="Back" />
+          </button>
+          <h1 title={countryDetail.name}>
+            {countryDetail.name || param.toLowerCase()}
+          </h1>
+        </div>
+        {error.state ? (
+          <InfoSign message={error.msg} />
+        ) : loading ? (
+          <Loader />
+        ) : (
+          <CountryDetail info={countryDetail} parameter={param} />
+        )}
+      </div>
     </div>
   );
 }
