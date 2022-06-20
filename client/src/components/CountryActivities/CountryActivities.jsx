eeadbbedefd3544e02f43e4e82f298cd9b3e1d13
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-20 02:50:21 -0300

    feat - establish a layout for the activities of the countries

--- client/src/components/CountryActivities/CountryActivities.jsx
+++ client/src/components/CountryActivities/CountryActivities.jsx
@@ -1,16 +1,33 @@
+import { InfoSign } from "../InfoSign/InfoSign";
+import no_activities_icon from "../../assets/img/no-activities.png";
+//styles
+import { activity_card, activity_info } from "./CountryActivities.module.css";
+import duration_icon from "../../assets/img/activities_time.png";
+import season_icon from "../../assets/img/activities_season.png";
+import difficulty_icon from "../../assets/img/activities_difficulty.png";
+
 export function CountryActivities({ activities }) {
   return activities ? (
     activities.length === 0 ? (
       <div>
-        <h3>No activities</h3>
+        <InfoSign title="No Activities" img={no_activities_icon} />
       </div>
     ) : (
       activities.map((act) => (
-        <div key={act.id}>
-          <h3>Name: {act.name}</h3>
-          <h3>Season: {act.season}</h3>
-          <h3>Difficulty: {act.difficulty}</h3>
-          <h3>Duration: {act.duration}</h3>
+        <div key={act.id} className={activity_card}>
+          <h3>{act.name}</h3>
+          <div className={activity_info}>
+            <img src={season_icon} alt="Season" title="season" />
+            <span>{act.season}</span>
+          </div>
+          <div className={activity_info}>
+            <img src={difficulty_icon} alt="Difficulty" title="difficulty" />
+            <span>{act.difficulty}</span>
+          </div>
+          <div className={activity_info}>
+            <img src={duration_icon} alt="Duration" title="duration" />
+            <span>{act.duration}</span>
+          </div>
         </div>
       ))
     )
