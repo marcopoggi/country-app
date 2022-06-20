574bc4af13ca894cddcb66b3313a80f520956384
Marco Poggi <marcopoggi2017@gmail.com>
2022-06-20 19:21:04 -0300

    add container for when there are no activities

--- client/src/components/PreferenceSection/PreferenceSection.jsx
+++ client/src/components/PreferenceSection/PreferenceSection.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 //styles
 import {
   preference_section_container,
@@ -6,7 +8,9 @@ import {
   descendent_btn,
   descendent_btn_true,
   descendent_btn_false,
+  section_no_data,
 } from "./PreferenceSection.module.css";
+import add_icon from "../../assets/img/add-icon.png";
 
 export function PreferenceSection({
   icon,
@@ -18,6 +22,7 @@ export function PreferenceSection({
   defaultValue = false,
   isDescendent,
 }) {
+  const navigate = useNavigate();
   return (
     <div className={preference_section_container}>
       <div className={preference_header}>
@@ -42,32 +47,43 @@ export function PreferenceSection({
         )}
       </div>
 
-      {data.map((val, index) => (
-        <div key={val} className={preference_section_inputs}>
-          {
-            //if you want to set a default value, select the first one.
-            defaultValue && index === 0 ? (
-              <input
-                type={type}
-                name={name}
-                value={val}
-                id={val}
-                onClick={handler}
-                defaultChecked
-              />
-            ) : (
-              <input
-                type={type}
-                name={name}
-                value={val}
-                id={val}
-                onClick={handler}
-              />
-            )
-          }
-          <label htmlFor={val}>{val}</label>
+      {data.length > 0 ? (
+        data.map((val, index) => (
+          <div key={val} className={preference_section_inputs}>
+            {
+              //if you want to set a default value, select the first one.
+              defaultValue && index === 0 ? (
+                <input
+                  type={type}
+                  name={name}
+                  value={val}
+                  id={val}
+                  onClick={handler}
+                  defaultChecked
+                />
+              ) : (
+                <input
+                  type={type}
+                  name={name}
+                  value={val}
+                  id={val}
+                  onClick={handler}
+                />
+              )
+            }
+            <label htmlFor={val}>{val}</label>
+          </div>
+        ))
+      ) : (
+        <div className={section_no_data}>
+          <h3>No {title}</h3>
+          {title === "Activities" ? (
+            <button onClick={() => navigate("/activities")}>
+              <img src={add_icon} alt="Create" />
+            </button>
+          ) : null}
         </div>
-      ))}
+      )}
     </div>
   );
 }
